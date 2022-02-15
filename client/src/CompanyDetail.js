0d6d13c1d7cb58b24738b10098541526d6aa06dd
mbentro <mbentro@gmail.com>
2022-02-15 16:06:17 -0700

    Returning Jobs for a Company

---
+++ client/src/CompanyDetail.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { loadCompany } from './requests';
+//import { companies } from './fake-data';
+
+
+export class CompanyDetail extends Component {
+  constructor(props) {
+    super(props);    
+    this.state = {company: null};
+  }
+
+  async componentDidMount(){
+    const {companyId} = this.props.match.params;
+    const company = await loadCompany(companyId);
+    this.setState({company});
+  }
+
+  render() {
+    const {company} = this.state;
+    if (!company) {
+      return null;
+    }
+    return (
+      <div>
+        <h1 className="title">{company.name}</h1>
+        <div className="box">{company.description}</div>
+      </div>
+    );
+  }
+}
---
+++ server/resolvers.js
@@ -0,0 +1,13 @@
+const db = require('./db');
+
+const Query = {
+    company: (root, {id}) => db.companies.get(id),
+    job: (root, args) => db.jobs.get(args.id),
+    jobs: () => db.jobs.list()
+};
+
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+};
+
+module.exports = { Query, Job };
